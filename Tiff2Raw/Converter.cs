4870029940e9a2a8e164643d8d3fb8d7a66c2669
Benn Herrera <bherrera@reification.io>
2019-05-23 19:16:35 -0700

    progress on geotiff to raw

---
+++ Tiff2Raw/Converter.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.IO;
+
+using BitMiracle.LibTiff.Classic;
+
+namespace Tiff2Raw {
+	public class Converter {
+		public string inputTiffPath = null;
+		public string outputRawPath = null;
+
+		public bool Go() {
+			try {
+				go();
+				return true;
+			} catch (Exception ex) {
+				log("Exception caught:\n{0}", ex.Message);
+			}
+			return false;
+		}
+
+		static void log(string message, params Object[] args) {
+			Console.WriteLine(string.Format(message, args));
+		}
+
+		static void warn(string message, params Object[] args) {
+			Console.WriteLine(string.Format(message, args));
+		}
+
+		static void error(string message, params Object[] args) {
+			throw new Exception(string.Format(message, args));
+		}
+
+		struct dv3 {
+			public double x;
+			public double y;
+			public double z;
+		}
+
+		struct TiePoint {
+			public dv3 rasterPt;
+			public dv3 modelPt;
+		}
+
+		enum GeoTiffTag {
+			MODELPIXELSCALETAG = 33550,
+			MODELTIEPOINTTAG = 33922,
+			MODELTRANSFORMATIONTAG = 34264,
+			GEOKEYDIRECTORYTAG = 34735,
+			GEODOUBLEPARAMSTAG = 34736,
+			GEOASCIIPARAMSTAG = 34737,
+			GDALNODATATAG = 42113
+		}
+
+		class ExtendedTagMethods : TiffTagMethods {
+			public ExtendedTagMethods() {
+			}
+
+			public override FieldValue[] GetField(Tiff tif, TiffTag tag) {
+				GeoTiffTag gtag = (GeoTiffTag)(int)tag;
+				FieldValue[] rawResult = base.GetField(tif, (TiffTag)(int)gtag);
+				FieldValue[] result = null;
+
+				switch ((int)tag) {
+				case (int)GeoTiffTag.MODELPIXELSCALETAG:
+					result = rawResult;
+					break;
+				case (int)GeoTiffTag.MODELTIEPOINTTAG:
+					result = rawResult;
+					break;
+				case (int)GeoTiffTag.MODELTRANSFORMATIONTAG:
+					result = rawResult;
+					break;
+				case (int)GeoTiffTag.GEOKEYDIRECTORYTAG:
+					result = rawResult;
+					break;
+				case (int)GeoTiffTag.GEODOUBLEPARAMSTAG:
+					result = rawResult;
+					break;
+				case (int)GeoTiffTag.GEOASCIIPARAMSTAG:
+					result = rawResult;
+					break;
+				case (int)GeoTiffTag.GDALNODATATAG:
+					result = rawResult;
+					break;
+				default:
+					result = rawResult;
+					break;
+				}
+
+				return result;
+			}
+		}
+
+		static dv3 getModelPixelScale(Tiff tif) {
+			FieldValue[] v = tif.GetField((TiffTag)(int)GeoTiffTag.MODELPIXELSCALETAG);
+			if (v?.Length == 2 && v[0].ToInt() == 3) {
+				double[] arr = v[1].ToDoubleArray();
+				var val = new dv3 { x = arr[0], y = arr[1], z = arr[2] };
+				return val;
+			}
+			return new dv3 { x = 0, y = 0, z = 0 };
+		}
+		static TiePoint[] getModelTiePoints(Tiff tif) {
+			FieldValue[] v = tif.GetField((TiffTag)(int)GeoTiffTag.MODELTIEPOINTTAG);
+			if (v?.Length == 2 && v[0].ToInt() > 0 && (v[0].ToInt() % 6) == 0) {
+				double[] arr = v[1].ToDoubleArray();
+				TiePoint[] pts = new TiePoint[arr.Length / 6];
+				for ( int i = 0, j = 0; i < pts.Length; i+=1, j+=6 ) {
+					pts[i].rasterPt = new dv3 { x = arr[j + 0], y = arr[j + 1], z = arr[j + 2] };
+					pts[i].modelPt = new dv3 { x = arr[j + 3], y = arr[j + 4], z = arr[j + 5] };
+				}
+				return pts;
+			}
+			return null;
+		}
+
+		static double[] getModelTransformation(Tiff tif) {
+			FieldValue[] v = tif.GetField((TiffTag)(int)GeoTiffTag.MODELTRANSFORMATIONTAG);
+			if (v?.Length == 2 && v[0].ToInt() == 16) {
+				var val = v[1].ToDoubleArray();
+				return val;
+			}
+			return null;
+		}
+
+		static short[] getGeoKeyDirectory(Tiff tif) {
+			FieldValue[] v = tif.GetField((TiffTag)(int)GeoTiffTag.GEOKEYDIRECTORYTAG);
+			if (v?.Length == 2 && v[0].ToInt() >= 4) {
+				var val = v[1].ToShortArray();
+				var strVal = v[1].ToString();
+				return val;
+			}
+			return null;
+		}
+
+		static double[] getGeoDoublesParams(Tiff tif) {
+			FieldValue[] v = tif.GetField((TiffTag)(int)GeoTiffTag.GEODOUBLEPARAMSTAG);
+			if (v?.Length == 2 && v[0].ToInt() > 0) {
+				var val = v[1].ToDoubleArray();
+				return val;
+			}
+			return null;
+		}
+
+		static string getGeoAsciiParams(Tiff tif) {
+			FieldValue[] v = tif.GetField((TiffTag)(int)GeoTiffTag.GEOASCIIPARAMSTAG);
+			if (v?.Length >= 2) {
+				string ascii = v[1].ToString();
+				return ascii;
+			}
+			return null;
+		}
+
+		static int getGdalNoData(Tiff tif) {
+			FieldValue[] v = tif.GetField((TiffTag)(int)GeoTiffTag.GDALNODATATAG);
+			if (v?.Length > 1) {
+				int val = int.Parse(v[1].ToString());
+				return val;
+			}
+			return int.MinValue;
+		}
+
+		void extenderProc(Tiff t) {
+			t.SetTagMethods(new ExtendedTagMethods());
+		}
+
+		void go() {
+			// just useful for examining extended values. not necessary.
+			Tiff.SetTagExtender((Tiff t) => { this.extenderProc(t); });
+
+			using (Tiff inImage = Tiff.Open(inputTiffPath, "r")) {
+				using (var stdout = Console.OpenStandardOutput()) {
+					inImage.PrintDirectory(stdout, TiffPrintFlags.NONE);
+				}
+
+				int width = inImage.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
+				int height = inImage.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
+
+				dv3 scale = getModelPixelScale(inImage);
+				TiePoint[] tiePoints = getModelTiePoints(inImage);
+				short[] geoKeys = getGeoKeyDirectory(inImage);
+				double[] doubleParams = getGeoDoublesParams(inImage);
+				string asciiParams = getGeoAsciiParams(inImage);
+				int noDataValue = getGdalNoData(inImage);
+
+				float minVal = (float)inImage.GetField(TiffTag.SMINSAMPLEVALUE)[0].ToDouble();
+				float maxVal = (float)inImage.GetField(TiffTag.SMAXSAMPLEVALUE)[0].ToDouble();
+				float maxScale = (float)(ushort.MaxValue / inImage.GetField(TiffTag.SMAXSAMPLEVALUE)[0].ToDouble());
+
+				var rawRaster = new ushort[width * height];
+				var srcRow = new float[width];
+				var dstRowIdx = 0;
+
+				for ( int y = 0; y < height; y++, dstRowIdx += width ) {
+					// read row!
+					for (int x = 0; x < width; x++) {
+						rawRaster[dstRowIdx + x] = (ushort)((srcRow[x] - minVal) * maxScale);
+					}
+				}
+
+				using (var outFile = new FileStream(outputRawPath, FileMode.Create, FileAccess.Write)) {
+					var rasterBytes = new byte[rawRaster.Length * 2];
+					Buffer.BlockCopy(rawRaster, 0, rasterBytes, 0, rawRaster.Length);
+					outFile.Write(rasterBytes, 0, rasterBytes.Length);
+				}
+			}
+		}
+	}
+}
---
+++ Tiff2Raw/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+
+namespace Tiff2Raw {
+	class Program {
+		static private string appName = "Tiff2Raw";
+
+		static private string[] usageText = {
+			appName + " <input.tif> [-overwrite] <output.raw>",
+			"  <input.tif>: source image",
+			"  -overwrite: if output image exists it will be overwritten. this is an error otherwise.",
+			"  <output.raw>: target raw image"
+		};
+
+		static void Main(string[] args) {
+			Converter cnv = new Converter();
+			bool overwriteOuput = false;
+
+			foreach(var arg in args) {
+				if ( arg[0] == '-' ) {
+					string option = arg.Substring(1).ToLower();
+					switch ( option ) {
+					case "h":
+					case "help":
+					case "u":
+					case "usage":
+						usage(null);
+						break;
+					case "overwrite":
+						overwriteOuput = true;
+						break;
+					}
+					continue;
+				}
+
+				if ( cnv.inputTiffPath == null ) {
+					cnv.inputTiffPath = arg;
+					continue;
+				}
+
+				if ( cnv.outputRawPath == null ) {
+					cnv.outputRawPath = arg;
+					continue;
+				}
+			}
+
+			if ( !(cnv.inputTiffPath?.Length > 0) || !(cnv.outputRawPath?.Length > 0) ) {
+				usage(args.Length > 0 ? "input and output must be specified." : null);
+			}
+
+			if(!File.Exists(cnv.inputTiffPath)) {
+				usage("{0} does not exist.", cnv.inputTiffPath);
+			}
+
+			{
+				var outputDir = Path.GetDirectoryName(cnv.outputRawPath);
+				if (outputDir.Length > 0 && !Directory.Exists(outputDir)) {
+					usage("{0} does not exist.", outputDir);
+				}
+			}
+
+			if (!overwriteOuput && File.Exists(cnv.outputRawPath)) {
+				usage("{0} exists. use different output path or specify -overwrite.", cnv.outputRawPath);
+			}
+
+			bool result = cnv.Go();
+
+			Environment.Exit(result ? 0 : 1);
+		}
+
+		private static void usage(string message, params object[] args) {
+			if (message != null) {
+				Console.WriteLine( string.Format(message, args));
+			}
+
+			foreach (var ln in usageText) {
+				Console.WriteLine(ln);
+			}
+
+			Environment.Exit(message == null ? 0 : -1);
+		}
+	}
+}
